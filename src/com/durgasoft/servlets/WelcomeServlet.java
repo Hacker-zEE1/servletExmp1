2b798d87c187166e8795d6290f6412578ed99767
mohds <mohds@FRida___AY>
2023-10-25 12:25:42 +0530

    First Commit

---
+++ src/com/durgasoft/servlets/WelcomeServlet.java
@@ -0,0 +1,25 @@
+package com.durgasoft.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class WelcomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h1>");
+		out.println("Welcome To Eclipse-Weblogic Server Integration");
+		out.println("<hr></h1></body></html>");
+	}
+
+}
